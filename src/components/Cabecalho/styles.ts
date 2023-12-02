f6f2adb7f0f8fe1fd915c72fb2dcabf655ed0709
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-12-02 18:29:38 -0300

    styled-components added

---
+++ src/components/Cabecalho/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Title = styled.header`
+  background-color: ${(props) => props.theme.CorSecundaria};
+  color: ${(props) => props.theme.CorPrincipal};
+  text-align: center;
+  padding: 24px 0;
+`
+
+export default Title
